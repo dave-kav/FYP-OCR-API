7349cb9d052082289bdc92897481da346d23355d
Dave Kavanagh <david.j.kavanagh@mycit.ie>
2017-02-14 21:48:41 +0000

    server.py edited online with Bitbucket

--- server.py
+++ server.py
@@ -14,6 +14,6 @@ def do_ocr():
     text = ocr_obj.ocr()
     return text
 
-port = os.getenv('PORT', '5000')
+port = os.getenv('PORT', '8080')
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=int(port))
