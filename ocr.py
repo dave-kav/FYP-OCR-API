6d55081b2a916852396dfa7df0b70141c890dd04
DESKTOP-3S7TGHG\davyk <david.j.kavanagh@mycit.ie>
2017-02-14 21:46:40 +0000

    uploaded bluemix project

---
+++ ocr.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Author:         Dave Kavanagh
+                R00013469
+                david.j.kavanagh@mycit.ie
+
+Date:           31/01/2017
+
+Description:    Sandbox for testing and evaluating Image processing libraries
+                with a view to determining what is necessary in order to proceed
+                with the OCR element of the project.
+
+© Dave Kavanagh, 2017
+"""
+import sys
+import numpy as np
+import pytesseract as tess
+from PIL import Image, ImageFilter, ImageOps
+
+class OCR:
+    """
+    Class used to perform OCR
+    """
+
+    def __init__(self, input_file):
+        """
+        Constructor
+        :param input_file: file path passed from command line
+        """
+        self.input_file = input_file
+
+    def open_image(self):
+        """
+        Attempts to load an image from disk, converts to grayscale, and crops out unnecessary portion of the image.
+        """
+        global cropped_img
+        try:
+            self.img = Image.open(self.input_file)
+
+        except:
+            print "ERROR: Unable to load image!"
+            exit(1)
+
+    def detect_lines(self):
+        """
+        Detect lines in an image using the "Hough Transform" - NOT CURRENTLY USED!!
+        """
+        minLineLength = 5
+        maxLineGap = 1
+        img_arr = np.array(self.img)
+        lines = cv2.HoughLinesP(img_arr, 1, np.pi / 180, 100, minLineLength, maxLineGap)
+        for x1, y1, x2, y2 in lines[0]:
+            cv2.line(img_arr, (x1, y1), (x2, y2), (255, 255, 255), 2)
+
+        #convert ndarray back to image object
+        self.img = Image.fromarray(img_arr)
+
+    def process_image(self):
+        """
+        Treats the image, subjecting it to various steps in order to enhance the possibility of accurate text recognition.
+        """
+        gray_img = ImageOps.grayscale(self.img)
+
+        # crop desired region
+        #cropped_img = gray_img.crop((20, 500, 1200, 1720))
+        #cropped_img = gray_img.crop((900, 500, 1300, 820))
+        cropped_img = gray_img.crop((120, 1350, 2300, 3020))
+
+        img = cropped_img.filter(ImageFilter.UnsharpMask)
+        img = ImageOps.autocontrast(img)
+        img = img.filter(ImageFilter.GaussianBlur)
+        img = img.filter(ImageFilter.SHARPEN)
+
+        #convert from grayscale to pure black and white
+        self.img = img.point(lambda x: 0 if x<125 else 250, '1')
+
+    def detect_text(self):
+        """
+        Attempts to extract text from an image
+        """
+        self.text = tess.image_to_string(self.img)
+        if len(self.text) < 1:
+            self.text = "No text detected in this image!"
+
+    def ocr(self):
+        """
+        Perform OCR on an image
+        """
+        if self.input_file:
+            self.open_image()
+            self.process_image()
+            self.img.show()
+            self.detect_text()
+
+        return self.text
+
+if __name__ == "__main__":
+    file_path = sys.argv[1]
+    ocr_obj = OCR(file_path)
+    print ocr_obj.ocr()
---
+++ server.py
@@ -0,0 +1,19 @@
+import os, ocr
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return app.send_static_file('index.html')
+
+@app.route('/ocr')
+def do_ocr():
+    image = 'static/images/bet2.jpg'
+    ocr_obj = ocr.OCR(image)
+    text = ocr_obj.ocr()
+    return text
+
+port = os.getenv('PORT', '5000')
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=int(port))
