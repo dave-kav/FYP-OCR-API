96f32c42bf1f13c486d6f54a2340a33ab95cb73a
Ubuntu <ubuntu@ip-172-31-46-157.eu-west-1.compute.internal>
2017-03-25 19:53:37 +0000

    testing server for post

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 import os, ocr
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.debug = True
+CORS(app)
+
+app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
 
 @app.route('/')
 def hello_world():
@@ -11,12 +16,13 @@ def hello_world():
 def welcome():
     return app.send_static_file('index.html')
 
-@app.route('/ocr')
+@app.route('/ocr', methods=['GET', 'POST'])
 def do_ocr():
     image = 'static/images/bet2.jpg'
     ocr_obj = ocr.OCR(image)
     text = ocr_obj.ocr()
-    return text
+    data = {'time': '12:25', 'track': 'kempton', 'selection': 'Shergar', 'odds': '21/20'}
+    return jsonify(ocr=data)
 
 port = os.getenv('PORT', '8080')
 if __name__ == "__main__":
