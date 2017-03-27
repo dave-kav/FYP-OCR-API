8c8434170941a72281a2b8b7f5a2e24b44170ded
dave-kav <david.j.kavanagh@mycit.ie>
2017-03-27 20:39:52 +0100

    mapped all individual sections and created stake mapping

--- ocr.py
+++ ocr.py
@@ -7,13 +7,14 @@ Author:         Dave Kavanagh
 Date:           31/01/2017
 
 Description:    Receives image data via the constructor and performs ocr on 
-                specfically mapped portions of the image, returning the text 
+                specifically mapped portions of the image, returning the text
                 as JSON.
 
 © Dave Kavanagh, 2017
 """
-import sys
-#import cv2
+from io import BytesIO
+import re
+import base64
 import numpy as np
 import pytesseract as tess
 from PIL import Image, ImageFilter, ImageOps
@@ -28,7 +29,9 @@ class OCR:
         Constructor
         :param input_file: file path passed from command line
         """
-        self.input_file = input_file
+        #remove image encoding prefix
+        image_data = input_file[24:]
+        self.input_file = image_data
 
     def open_image(self):
         """
@@ -36,25 +39,21 @@ class OCR:
         """
         global cropped_img
         try:
-            self.img = Image.open(self.input_file)
+            self.img = Image.open(BytesIO(base64.b64decode(self.input_file)))
 
         except:
             print "ERROR: Unable to load image!"
             exit(1)
 
-    def detect_lines(self):
-        """
-        Detect lines in an image using the "Hough Transform" - NOT CURRENTLY USED!!
-        """
-        minLineLength = 5
-        maxLineGap = 1
-        img_arr = np.array(self.img)
-        #lines = cv2.HoughLinesP(img_arr, 1, np.pi / 180, 100, minLineLength, maxLineGap)
-        #for x1, y1, x2, y2 in lines[0]:
-        #    cv2.line(img_arr, (x1, y1), (x2, y2), (255, 255, 255), 2)
+    def treat_image(self, img):
+        img = img.filter(ImageFilter.UnsharpMask)
+        img = ImageOps.autocontrast(img)
+        img = img.filter(ImageFilter.GaussianBlur)
+        img = img.filter(ImageFilter.SHARPEN)
 
-        #convert ndarray back to image object
-        self.img = Image.fromarray(img_arr)
+        # convert from grayscale to pure black and white
+        img = img.point(lambda x: 0 if x < 125 else 250, '1')
+        return img
 
     def process_image(self):
         """
@@ -62,40 +61,86 @@ class OCR:
         """
         gray_img = ImageOps.grayscale(self.img)
 
-        # crop desired region
-        #cropped_img = gray_img.crop((20, 500, 1200, 1720))
-        #cropped_img = gray_img.crop((900, 500, 1300, 820))
-        cropped_img = gray_img.crop((120, 1350, 2300, 3020))
+        # crop desired regions
+        # time region
+        time_section = gray_img.crop((920, 630, 1600, 1020))
+        self.time_section = self.treat_image(time_section)
 
-        img = cropped_img.filter(ImageFilter.UnsharpMask)
-        img = ImageOps.autocontrast(img)
-        img = img.filter(ImageFilter.GaussianBlur)
-        img = img.filter(ImageFilter.SHARPEN)
+        # selection region
+        selection_section = gray_img.crop((920, 950, 1600, 1520))
+        self.selection_section = self.treat_image(selection_section)
 
-        #convert from grayscale to pure black and white
-        self.img = img.point(lambda x: 0 if x<125 else 250, '1')
+        # odds region
+        odds_section = gray_img.crop((920, 1550, 1600, 1970))
+        self.odds_section = self.treat_image(odds_section)#odds region
 
-    def detect_text(self):
+        # stake region
+        stake_section = gray_img.crop((1160, 2200, 1700, 2350))
+        self.stake_section = self.treat_image(stake_section)
+
+    def detect_text(self, img, section):
         """
         Attempts to extract text from an image
         """
-        self.text = tess.image_to_string(self.img)
-        if len(self.text) < 1:
-            self.text = "No text detected in this image!"
+        text = tess.image_to_string(img)
+        if len(text) < 1:
+            text = "No text detected in " + section + " section!"
 
-    def ocr(self):
+        return text
+
+    def analyze_bet(self):
         """
-        Perform OCR on an image
+        Perform OCR on betting slip
         """
         if self.input_file:
             self.open_image()
             self.process_image()
-            "self.img.show()"
-            self.detect_text()
 
-        return self.text
+            #detect time
+            #self.time_section.show()
+            time = self.detect_text(self.time_section, 'time')
+            if '.' in time:
+                time = time.replace('.', ':')
+            if '-' in time:
+                time = time.replace('-', ':')
+            if ';' in time:
+                time = time.replace(';', ':')
 
-if __name__ == "__main__":
-    file_path = sys.argv[1]
-    ocr_obj = OCR(file_path)
-    print ocr_obj.ocr()
+            #detect selection
+            #self.selection_section.show()
+            selection = self.detect_text(self.selection_section, 'selection')
+
+            # detect odds
+            #self.odds_section.show()
+            odds = self.detect_text(self.odds_section, 'odds')
+            if '.' in odds:
+                odds = odds.replace('.', '/')
+
+            ocr_data = {'time': time, 'selection': selection, 'odds': odds}
+
+        return ocr_data
+
+    def identify_stake(self):
+        """
+        Perform OCR on bet to identify stake
+        :return: JSON containing stake value
+        """
+        if self.input_file:
+            self.open_image()
+            self.process_image()
+
+            # detect stake
+            self.stake_section.show()
+            stake = self.detect_text(self.stake_section, 'stake')
+            print stake
+            if 'o' in stake:
+                stake = stake.replace('o', '0')
+            if 'O' in stake:
+                stake = stake.replace('O', '0')
+
+            ocr_data = {'stake': stake}
+
+        return ocr_data
+
+
+
--- server.py
+++ server.py
@@ -1,45 +1,51 @@
 import os, ocr
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from OpenSSL import SSL
 
 app = Flask(__name__)
-app.debug = True
+#disable csrf protection
+
 CORS(app)
 
-def _force_https():
-    from flask import _request_ctx_stack
-    if _request_ctx_stack is not None:
-        reqctx = _request_ctx_stack.top
-        reqctx.url_adapter.url_scheme = 'https'
-
+#increase max file upload limit
 app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
-app.before_request(_force_https)
+
+#enable ssl to serve over https
 context = ('cert.crt','key.key')
 
-@app.route('/')
-def hello_world():
-    return 'Hello!'
-
-@app.route('/welcome')
-def welcome():
-    return app.send_static_file('index.html')
-
 @app.route('/ocr', methods=['GET', 'POST'])
-def do_ocr():
-	if request.method == 'POST':
-		if 'imgData' in request.values:
-			print 'image string POSTed to server!'
-		else:
-			print "image did not POST!"
+def analyze_bet():
+    if request.method == 'POST':
+        if 'imgData' in request.values:
+            ocr_obj = ocr.OCR(request.values['imgData'])
+            data = ocr_obj.analyze_bet()
+            print data
+            return jsonify(ocr=data)
+        else:
+            data = {'time': '', 'track': '', 'selection': '', 'odds': ''}
+            return jsonify(ocr=data)
+    else:
+        #dummy data for dev
+        data = {'time': '14:35', 'track': 'Galway', 'selection': 'Kauto Star', 'odds': '21/20'}
+        return jsonify(ocr=data)
 
-	image = 'static/images/bet2.jpg'
-	ocr_obj = ocr.OCR(image)
-	text = ocr_obj.ocr()
-	data = {'time': '14:35', 'track': 'Galway', 'selection': 'Kauto Star', 'odds': '21/20'}
-	return jsonify(ocr=data)
+@app.route('/ocr/stake', methods=['GET', 'POST'])
+def identify_stake():
+    if request.method == 'POST':
+        if 'imgData' in request.values:
+            ocr_obj = ocr.OCR(request.values['imgData'])
+            data = ocr_obj.identify_stake()
+            print data
+            return jsonify(ocr=data)
+        else:
+            data = {'stake': ''}
+            return jsonify(ocr=data)
+    else:
+        #dummy data for dev
+        data = {'stake': '25.00'}
+        return jsonify(ocr=data)
 
-port = os.getenv('PORT', '443')
+port = os.getenv('PORT', '8081')
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=int(port), ssl_context=context)
+    app.run(host='0.0.0.0', port=int(port))
 
